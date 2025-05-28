ffb3458c683c9e81f1157cc2469f8c4f579c5457
Ethan Saintot <ethan.saintot@epitech.eu>
2025-05-28 13:31:58 +0200

    add gray window with more settings

--- menu.py
+++ menu.py
@@ -7,5 +7,11 @@
 from tkinter import *
 
 window_menu = Tk()
+window_menu.title("3d python game")
+window_menu.geometry("1280x720")
+window_menu.resizable(False, False)
+window_menu.configure(bg="#b9bdb3")
+
+#welcome_msg = Label(window_menu, text = "Welcome to my 3d python game", )
 
 window_menu.mainloop()
