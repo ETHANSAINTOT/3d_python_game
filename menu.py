44610d410d29e4e2c8d5689d3a714de9cfd22908
Ethan Saintot <ethan.saintot@epitech.eu>
2025-05-28 13:35:55 +0200

    add a text to window

--- menu.py
+++ menu.py
@@ -12,6 +12,7 @@ window_menu.geometry("1280x720")
 window_menu.resizable(False, False)
 window_menu.configure(bg="#b9bdb3")
 
-#welcome_msg = Label(window_menu, text = "Welcome to my 3d python game", )
+welcome_msg = Label(window_menu, text = "Welcome to my 3d python game", font=('Arial', 40), fg="white", bg="#b9bdb3")
+welcome_msg.pack(side = "top", padx = 10, pady=10)
 
 window_menu.mainloop()
